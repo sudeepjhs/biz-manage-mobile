26317ba6c51d8458db1c727856c2ba865ae2316e
Sudeep Agarwal <sudeepagarwaljhs@gmail.com>
2026-03-29 01:18:10 +0530

    feat: implement native splash screen for Android using androidx.core:core-splashscreen

--- android/app/src/main/java/com/BizManageMobile/MainActivity.kt
+++ android/app/src/main/java/com/BizManageMobile/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.BizManageMobile
 
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,16 +9,21 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "BizManageMobile"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()   // <-- added: system splash before React Native boots
+        super.onCreate(savedInstanceState)
+    }
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "BizManageMobile"
+
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flag [fabricEnabled].
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
